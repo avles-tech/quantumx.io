53609bfedf218d346287fe36b82f69ef3ffb46b8
selva <selvakumar@ecyber.com>
2023-05-31 15:57:33 +0530

    remove sticky

--- app/admin/QAppBar.tsx
+++ app/admin/QAppBar.tsx
@@ -1,4 +1,4 @@
 import { AppBar } from 'react-admin';
 import { Typography } from '@mui/material';
 
-export const QAppBar = () => <AppBar color="primary" position="sticky" />;
+export const QAppBar = () => <AppBar color="primary"  />;
