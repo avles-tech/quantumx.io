0676946d10589302cdde714e9ead708d9994b709
selva <selvakumar@ecyber.com>
2023-06-01 22:35:01 +0530

    appbar img to image

--- app/admin/QAppBar.tsx
+++ app/admin/QAppBar.tsx
@@ -2,6 +2,7 @@ import { AppBar, Title, TitlePortal } from 'react-admin';
 import { Search } from '@mui/icons-material';
 import { Box, IconButton } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
+import Image from 'next/image'; // Import Image from next/image
 
 const SettingsButton = () => (
     <IconButton color="inherit">
@@ -12,8 +13,13 @@ const SettingsButton = () => (
 export const QAppBar = () => (
     <AppBar color="primary">
         <Box display="flex" alignItems="center">
-            <img src="https://flowbite.com/docs/images/logo.svg" alt="Quantumx.io" style={{ maxHeight: '35px', marginRight: '20px' }}
-            height={40} />
+            {/* Replace the img tag with the Next.js Image component */}
+            <Image
+                src="https://flowbite.com/docs/images/logo.svg"
+                alt="Quantumx.io"
+                width={60}  // replace with the desired width
+                height={35}  // replace with the desired height
+            />
 
             <div> Quantumx.io </div></Box>
 
