73bd7ef986c77efa6e23553f0531cb08cbc6629d
selva <selvakumar@ecyber.com>
2023-06-04 09:37:58 +0530

    leave type

--- app/admin/leaveModels.tsx
+++ app/admin/leaveModels.tsx
@@ -6,6 +6,29 @@ const LeaveModelsFilter = (props: any) => (
     </Filter>
   );
 
+  import { ArrayInput, SimpleFormIterator, NumberInput, SelectInput } from "react-admin";
+
+const LeaveModelForm = (props: any) => (
+    <SimpleForm>
+        <TextInput source="shortCode" />
+        <TextInput source="details" />
+        <BooleanInput source="active" />
+
+        <ArrayInput source="leaveTypes">
+            <SimpleFormIterator>
+                <ReferenceInput source="leaveTypeId" reference="leaveTypes" label='Leave Type'>
+                    <SelectInput optionText="details" />
+                </ReferenceInput>
+                <NumberInput source="noOfLeaves" label="Number of Leaves" />
+            </SimpleFormIterator>
+        </ArrayInput>
+    </SimpleForm>
+);
+
+// use the form in Create and Edit as before
+
+
+
 
 export const LeaveModelList = () => (
     <>
@@ -22,12 +45,7 @@ export const LeaveModelList = () => (
 
 export const LeaveModelEdit = () => (
     <Edit>
-        <SimpleForm>
-            <TextInput source="id" disabled />
-            <TextInput source="shortCode" />
-            <TextInput source="details" />
-            <BooleanInput source="active" />
-        </SimpleForm>
+        <LeaveModelForm/>
     </Edit>
 );
 
