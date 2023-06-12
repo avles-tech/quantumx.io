fc9ecba8e454d0cd07310f25765fce5015a10e5b
selva <selvakumar@ecyber.com>
2023-06-12 05:35:11 +0530

    changes

--- app/admin/attendanceLog.tsx
+++ app/admin/attendanceLog.tsx
@@ -1,29 +1,32 @@
-import { List, Datagrid, TextField, BooleanField, Edit, SimpleForm, TextInput, ReferenceInput, BooleanInput, Create, Filter, DateTimeInput, DateField } from "react-admin";
+import { List, Datagrid, TextField, BooleanField, Edit, SimpleForm, TextInput, ReferenceInput, BooleanInput, Create, Filter, DateTimeInput, DateField, ReferenceField } from "react-admin";
 
 const AttendanceLogFilter = (props: any) => (
   <Filter {...props}>
     <TextInput label="Search" source="q" alwaysOn inputProps={{ style: { width: '500px' } }} />
+    <DateTimeInput source="dateTime_gte" label="Date & Time from" />
+    <DateTimeInput source="dateTime_lte" label="Date & Time to" />
   </Filter>
 );
 
 const AttendanceLogForm = (props: any) => (
   <SimpleForm>
     <TextInput source="batchId" label='Batch ID' />
-    <TextInput source="cardId" label='Card ID'/>
+    <TextInput source="cardId" label='Card ID' />
     <DateTimeInput
       source="dateTime"
       label="Date & Time"
       type="datetime-local"
       inputProps={{
-        format: 'dd:MM:yyyy' , placeholder: 'dd:MM:yyyy',
+        placeholder: 'yyyy-MM-ddThh:mm',
         ampm: false,
       }}
     />
-    <TextInput source="deviceId" label='Device ID'/>
-    <TextInput source="attdType" label='Attendance Type'/>
-    <TextInput source="trtRecord" label='TRT Record'/>
+    <TextInput source="deviceId" label='Device ID' />
+    <TextInput source="attdType" label='Attendance Type' />
+    <TextInput source="trtRecord" label='TRT Record' />
   </SimpleForm>
 );
+
 
 export const AttendanceLogList = () => (
   <>
@@ -31,7 +34,10 @@ export const AttendanceLogList = () => (
     <List filters={<AttendanceLogFilter />}>
       <Datagrid rowClick="edit">
         <TextField source="batchId" label='Batch ID' />
-        <TextField source="cardId" label='Card ID'/>
+        <TextField source="cardId" label='Card ID' />
+        <ReferenceField source="cardId"  reference="employees">
+          <TextField source="cardId" />
+        </ReferenceField>
         <DateField
           source="dateTime"
           showTime
@@ -39,10 +45,11 @@ export const AttendanceLogList = () => (
           label="Date & Time"
           locale="en-GB"
           options={{ day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false }}
-          />            
-        <TextField source="deviceId" label='Device ID'/>
-        <TextField source="attdType" label='Attendance Type'/>
-        <TextField source="trtRecord" label='TRT Record'/>
+        />
+       
+        <TextField source="deviceId" label='Device ID' />
+        <TextField source="attdType" label='Attendance Type' />
+        <TextField source="trtRecord" label='TRT Record' />
       </Datagrid>
     </List>
   </>
